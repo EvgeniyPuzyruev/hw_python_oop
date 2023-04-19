3cf2a6477ed81d78effbb02d989e5f77b46a4465
Евгений Пузырёв <puzyrevea@gmail.com>
2023-04-19 17:35:04 +0700

    ff2

--- homework.py
+++ homework.py
@@ -9,10 +9,10 @@ class InfoMessage:
 
     def get_message(self):
         return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {round(self.duration, 3)}; '
-                f'Дистанция: {round(self.distance, 3)}; '
-                f'Ср. скорость: {round(self.speed, 3)}; '
-                f'Потрачено ккал: {round(self.calories, 3)}'
+                f'Длительность: {self.duration, 3}; '
+                f'Дистанция: {self.distance, 3}; '
+                f'Ср. скорость: {self.speed, 3}; '
+                f'Потрачено ккал: {self.calories, 3}'
                 )
 
 
